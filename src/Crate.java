b980b6b83348d85f9f2c45ba8f2b6ec89070bb8e
Tcartegnie <Sorakssis@gmail.com>
2025-08-12 16:52:26 +0200

    Create Crate.java

---
+++ src/Crate.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Crate {
+private String name;
+private String [] items;
+
+
+public static void crate(String[] args)
+    {
+
+    }
+
+    public void AddItem(String item)
+    {
+        items[items.length-1] = item;
+    }
+
+    public void RemoveItem(String item)
+    {
+        items[items.length-1] = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getItems() {
+        return items;
+    }
+
+    public void setItems(String[] items) {
+        this.items = items;
+    }
+}
